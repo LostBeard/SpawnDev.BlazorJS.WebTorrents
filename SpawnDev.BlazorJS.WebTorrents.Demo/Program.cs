019d4f2fd93939eb1a1b983978566960e6334037
Todd Tanner <todd@spawndev.com>
2024-03-26 21:17:47 -0400

    Removed unnecessary service

--- SpawnDev.BlazorJS.WebTorrents.Demo/Program.cs
+++ SpawnDev.BlazorJS.WebTorrents.Demo/Program.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SpawnDev.BlazorJS;
-using SpawnDev.BlazorJS.Toolbox;
 using SpawnDev.BlazorJS.WebTorrents;
 using SpawnDev.BlazorJS.WebTorrents.Demo;
 
@@ -10,6 +9,5 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddBlazorJSRuntime();
-builder.Services.AddSingleton<BeforeUnloadService>();
 builder.Services.AddSingleton<WebTorrentService>();
 await builder.Build().BlazorJSRunAsync();
