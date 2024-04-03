abb1865c360c3e38f341670d5a391f3468bcb865
Todd Tanner <todd@spawndev.com>
2024-04-03 00:37:06 -0400

    Updated total seeds count

--- SpawnDev.BlazorJS.WebTorrents.Demo/Shared/TorrentsDataGridItem.cs
+++ SpawnDev.BlazorJS.WebTorrents.Demo/Shared/TorrentsDataGridItem.cs
@@ -16,8 +16,9 @@ namespace SpawnDev.BlazorJS.WebTorrents.Demo.Shared
                 );
             }
         }
-        public int TrackerSeeders => Torrent.Announced?.Values.Where(o => !o.Expired).Sum(o => o.Complete) ?? 0;
+        public int TrackerSeeders => (Torrent.Announced?.Values.Where(o => !o.Expired).Sum(o => o.Complete) ?? 0) + WebSeeds;
         public int TrackerPeers => Torrent.Announced?.Values.Where(o => !o.Expired).Sum(o => o.Incomplete) ?? 0;
+        public int WebSeeds => Torrent.Wires.Where(o => o.Type == "webSeed").Count();
         public double UploadSpeed => Torrent.UploadSpeed;
         public double TimeRemaining => Torrent.TimeRemaining ?? TimeSpan.MaxValue.TotalSeconds + 1d;
         public double DownloadSpeed => Torrent.DownloadSpeed;
