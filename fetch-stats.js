769a5ef433f6e563c781bc413417fae80c18f9f4
LostBeard <LostBeard@users.noreply.github.com>
2025-10-24 13:17:48 +0000

    Deploying to gh-pages from @ LostBeard/SpawnDev.BlazorJS.WebTorrents@b93e1aeb6d8256d1baf0a61e1cda871693f07a4f 🚀

--- fetch-stats.js
+++ fetch-stats.js
@@ -139,33 +139,46 @@
             }
         }
     };
-    //var useWebSocketProxy = false;
-    //var WebSocketOrig = WebSocket;
-    //var blockedHostTarget = 'wss://localhost:65535/host_blocked';
-    //var WebSocket = function (wsUrl, protocols) {
-    //    var url = new URL(wsUrl);
-    //    var hostname = url.hostname;
-    //    var isBlocked = fetchStats.isHostBlocked(hostname);
-    //    //console.log('new WebSocket', hostname, wsUrl, protocols);
-    //    if (isBlocked) {
-    //        //console.log('Blocked WebSocket connection to host', hostname);
-    //        wsUrl = blockedHostTarget;
-    //    }
-    //    var ws = new WebSocketOrig(wsUrl, protocols);
-    //    if (!useWebSocketProxy) return ws;
-    //    var overrides = {
-    //        //
-    //    };
-    //    var wsp = new Proxy(ws, {
-    //        get(target, key) {
-    //            if (key in overrides) {
-    //                return overrides[key];
-    //            }
-    //            var ret = Reflect.get(target, key, this);
-    //            return ret;
-    //        }
-    //    });
-    //    return wsp;
-    //}
-    //globalThis.WebSocket = WebSocket;
+    class WebSocketExtEvent extends Event {
+        constructor(type, extSocket) {
+            super(type);
+            this.socket = extSocket;
+        }
+    }
+    class WebSocketExtNewEvent extends WebSocketExtEvent {
+        constructor(extSocket) {
+            super('newsocket', extSocket);
+        }
+    }
+    class WebSocketExtSendEvent extends WebSocketExtEvent {
+        constructor(extSocket, data) {
+            super('send', extSocket);
+            this.data = data;
+            this.cancelSend = false;
+        }
+    }
+    var webSocketExtId = 0;
+    class WebSocketExt extends WebSocket {
+        constructor(wsUrl, protocols) {
+            var url = new URL(wsUrl);
+            var hostname = url.hostname;
+            var isBlocked = fetchStats.isHostBlocked(hostname);
+            if (isBlocked) {
+                wsUrl = WebSocketExt.blockedHostTarget;
+            }
+            super(wsUrl, protocols);
+            this.extId = webSocketExtId++;
+            globalThis.dispatchEvent(new WebSocketExtNewEvent(this));
+        }
+        send(data) {
+            var e = new WebSocketExtSendEvent(this, data);
+            this.dispatchEvent(e);
+            if (e.cancelSend) return;
+            super.send(e.data);
+        }
+        static blockedHostTarget = 'wss://localhost:65535/host_blocked';
+    }
+    globalThis.WebSocket = WebSocketExt;
+    // change the class name to match WebSocket
+    Object.defineProperty(globalThis.WebSocket, 'name', { value: 'WebSocket' })
 })();
